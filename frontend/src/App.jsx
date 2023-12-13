49adc14592fa9414102bf1ea5b4401edfe3eae9e
Muhammad Haroon <contactwithharoon@gmail.com>
2023-12-14 00:51:27 +0500

    write schemas in sanity + navbar (desktop) in react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navbar } from "./components";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Navbar.scss";
+import { images } from "../../constants";
+
+const Navbar = () => {
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="Code x Haroon" />
+      </div>
+
+      <ul className="app__navbar-links">
+        {["home", "about", "contact", "work", "skills"].map((item, index) => (
+          <li key={`link-${item}`} className="app__flex p-text">
+            <div></div>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
